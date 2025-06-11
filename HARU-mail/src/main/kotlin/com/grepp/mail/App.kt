550f07e1e1c5cbc23e9cc19c860b79f4bc0db2a7
eonwy <yeawon6566@gmail.com>
2025-06-11 15:09:44 +0900

    create mail server to 3rd project

---
+++ HARU-mail/src/main/kotlin/com/grepp/mail/App.kt
@@ -0,0 +1,11 @@
+package com.grepp.mail
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class HaruMailApplication
+
+fun main(args: Array<String>) {
+    runApplication<HaruMailApplication>(*args)
+}
