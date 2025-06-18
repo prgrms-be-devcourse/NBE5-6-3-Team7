f17d4a50ee730d875cf2b9b7fb3ca2a5fa65347d
Kimoon J <jkm3093@naver.com>
2025-06-18 09:31:23 +0900

    🐛 Prevent deleted keywords from displaying in diary view

--- HARU-service/src/main/java/com/grepp/diary/app/model/keyword/repository/KeywordRepository.java
+++ HARU-service/src/main/java/com/grepp/diary/app/model/keyword/repository/KeywordRepository.java
@@ -1,6 +1,7 @@
 package com.grepp.diary.app.model.keyword.repository;
 
 import com.grepp.diary.app.model.keyword.entity.Keyword;
+import java.util.List;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -9,4 +10,7 @@ import org.springframework.stereotype.Repository;
 public interface KeywordRepository extends JpaRepository<Keyword, Integer>, KeywordRepositoryCustom {
 
     Optional<Keyword> findByName(String name);
+
+    List<Keyword> findByIsUseTrue();
+
 }
